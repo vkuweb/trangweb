f3a9e08f14f8aa6edf4194cf9e69ad57965298fa
vkuweb <106548035+vkuweb@users.noreply.github.com>
2022-06-02 21:55:23 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+//animate (xử lý dlieu chuyen đong)
+	new WOW().init();
+// xử lý khối swiperjs
+	var swiper = new Swiper(".mySwiper",{
+		effect: "coverflow",
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: "auto",
+		coverflowEffect: {
+			rotate: 50,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true,
+		},
+		pagination: {
+			el: ".swiper-pagination",
+		},
+	});
+
+})
